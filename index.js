ac1e7eb9e9059b4e0140b6733542334a28143337
JD Tadlock <jt7903@gmail.com>
2023-06-23 09:18:17 -0400

    fin npm overview

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const funcs = require('./printName.js');
+// const dayjs = require('dayjs');
+const inquirer = require('inquirer');
 
-// funcs.asdfasdfasdf('Bob');
+inquirer.prompt([
+  {
+    name: 'num1',
+    message: 'Please enter your first number.',
+    type: 'number'
+  },
+  {
+    name: 'num2',
+    message: 'Please enter your second number.',
+    type: 'number'
+  },
+  {
+    name: 'operator',
+    message: 'Please choose the operation',
+    type: 'list',
+    choices: ['Add', 'Subtract', 'Multiply', 'Divide']
+  }
+]).then((answerObj) => {
+  switch (answerObj.operator) {
+    case 'Add':
+      console.log(answerObj.num1 + answerObj.num2);
+      break;
+    case 'Subtract':
+      console.log(answerObj.num1 - answerObj.num2);
+      break;
+    case 'Divide':
+      console.log(answerObj.num1 / answerObj.num2);
+      break;
+    case 'Multiply':
+      console.log(answerObj.num1 * answerObj.num2);
+      break;
+  }
 
-const cap = funcs.capitalize('some string');
 
-console.log(cap);
+});
